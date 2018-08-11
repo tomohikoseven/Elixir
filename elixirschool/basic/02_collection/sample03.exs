bcd8e7bcf33ab7b3dce16a61653cec06ba6f1bec
tomohikoseven <tomohikoseven0905@gmail.com>
2018-08-11 14:51:58 +0900

    elixir school basic collection.

---
+++ elixirschool/basic/02_collection/sample03.exs
@@ -0,0 +1,3 @@
+IO.puts hd [3.14, :pie, "Apple"]
+tail = tl [3.14, :pie, "Apple"]
+IO.puts Enum.at( tail, 0 )
---
+++ elixirschool/basic/02_collection/sample04.exs
@@ -0,0 +1,3 @@
+[head | tail ] = [ 3.14, :pie, "Apple" ]
+IO.puts head
+IO.puts Enum.at( tail, 0 )
